e49d967ec27efaa86046f616c6abab891e79bbd5
dockerandgit <158227684+dockerandgit@users.noreply.github.com>
2025-01-31 01:02:12 +0000

    replace

--- invokeai/frontend/web/src/app/store/constants.ts
+++
@@ -1,4 +0,0 @@
-export const STORAGE_PREFIX = '@@invokeai-';
-export const EMPTY_ARRAY = [];
-/** @knipignore */
-export const EMPTY_OBJECT = {};
--- tests/backend/lora/conversions/lora_state_dicts/utils.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-
-def keys_to_mock_state_dict(keys: list[str]) -> dict[str, torch.Tensor]:
-    state_dict: dict[str, torch.Tensor] = {}
-    for k in keys:
-        state_dict[k] = torch.empty(1)
-    return state_dict
